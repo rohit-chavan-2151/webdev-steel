672c427f08a7c52fa98dd33f462a73eaf081d139
Rohit Chavan <rc2151@gmail.com>
2022-09-16 11:31:51 +0530

    Basics of string

---
+++ 07-javascript-basics/README.md
@@ -0,0 +1,4 @@
+## Strings Basics
+
+- To use quotes inside of the string, use single quotes first, and then use double quotes inside of it wherever needed.
+- Strings are indexed. Each character has a corresponding index, a positional number.
---
+++ 07-javascript-basics/script.js
@@ -0,0 +1,20 @@
+// alert('Hello world!')
+//Primitive Types - Number, String, Boolean, Null, undefined
+
+let username = 'rohitchavan2151'
+console.log(username)
+
+//to use quotes inside of thr string
+let sentence = 'He asked, "Come here!"'
+console.log(sentence)
+
+console.log(username[3]) //i - third character in string
+console.log(username.length) //15
+
+let firstName = 'Steve'
+let lastName = 'Austin'
+let id = 316
+console.log(firstName + lastName) //SteveAustin - Concatenation
+console.log(firstName + ' ' + lastName) //Steve Austin
+console.log(typeof firstName)
+console.log(typeof id)
